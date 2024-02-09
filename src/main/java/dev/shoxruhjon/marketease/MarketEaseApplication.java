3f16c9c97d272618c01ffb26a12d1ddf0816d33e
Shohrukh <shoxruh9902@gmail.com>
2024-02-09 08:30:06 +0500

    Initial Project

---
+++ src/main/java/dev/shoxruhjon/marketease/MarketEaseApplication.java
@@ -0,0 +1,13 @@
+package dev.shoxruhjon.marketease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketEaseApplication.class, args);
+    }
+
+}
